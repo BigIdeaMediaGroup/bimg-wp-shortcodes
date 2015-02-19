04d76a2e9f1e83f2da87f7a1bd9861ee955f7fe4
Will Clardy <will@quexxon.net>
2015-02-19 18:37:42 -0500

    Created bimg-wp-shortcodes.php

---
+++ bimg-wp-shortcodes.php
@@ -0,0 +1,10 @@
+<?php
+/**
+ * Plugin Name: BIMG WP Shortcodes
+ * Plugin URI: https://github.com/BigIdeaMediaGroup/bimg-wp-shortcodes
+ * Description: A set of general plugins for use in BIMG projects
+ * Version: 0.1
+ * Author: Will Clardy
+ * Author URI: https://github.com/quexxon
+ * License: GPL2
+ */
