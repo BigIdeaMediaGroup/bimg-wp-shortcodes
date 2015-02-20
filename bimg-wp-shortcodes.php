f213459e5a5f824d9ce6d64a331eafc4b8865f0d
Will Clardy <will@quexxon.net>
2015-02-20 00:07:30 -0500

    Initialization of unit testing utilities

--- bimg-wp-shortcodes.php
+++ bimg-wp-shortcodes.php
@@ -2,9 +2,25 @@
 /**
  * Plugin Name: BIMG WP Shortcodes
  * Plugin URI: https://github.com/BigIdeaMediaGroup/bimg-wp-shortcodes
- * Description: A set of general plugins for use in BIMG projects
+ * Description: A set of general plugins for use in BIMG projects.
  * Version: 0.1
  * Author: Will Clardy
  * Author URI: https://github.com/quexxon
  * License: GPL2
+ *
+ * Copyright 2015 Will Clardy <will@quexxon.net>
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License, version 2, as
+ * published by the Free Software Foundation.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
  */
+
