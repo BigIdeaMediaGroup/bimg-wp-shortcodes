9872241053ce9dc8b8f9ec606484f2871c5576b1
ChrisBurgin <chrisburgin95@gmail.com>
2015-03-05 14:30:23 -0500

    Code sniffer Run
    Passthrough of code sniffer on buttons function

--- shortcodes/bimg-button.php
+++ shortcodes/bimg-button.php
@@ -3,30 +3,24 @@
 		ShortCode Options:
 			type: rounded | square (default: rounded)
 			size: small | medium | large (default: medium)
-			border: true | flase (default: false)	
+            border: true | flase (default: false)
 			text: "button Text"
 			text_color: "text color"
 			button_color: "button color"
 			link: "button Link"
 			class: "extra custom class"
-			
-			
-		
-		
-		
 	*/
 class BIMGButton {
 	public function __construct()
 	{
 		add_action( 'wp_enqueue_scripts', array( $this, 'enqueue_shortcode_scripts' ) );
 		add_shortcode( 'bimg_button', array( $this, 'shortcode' ) );
-	} 
-	
+	}
+
 	public function enqueue_shortcode_scripts()
 	{
 		wp_enqueue_style( 'pure', plugins_url( 'bimg-wp-shortcodes/css/button/pure.css' ) );
 		wp_enqueue_style( 'button', plugins_url( 'bimg-wp-shortcodes/css/button/button.css' ) );
-		
 
 	}
 
@@ -40,33 +34,31 @@ class BIMGButton {
 				'button_color' => null,
 				'link' => null,
 				'class' => null,
-				
+
 			), $atts, 'bimg_button' );
 
-			if (null != $a['text']) //Conditional check for 'text'
-			{
-			$output = '<a ';
-			$output .= 'class="pure-button '
+			if ( null != $a['text'] ) {
+				$output = '<a ';
+				$output .= 'class="pure-button '
 					. 'button_' . $a['type']
 					. ' button_' . $a['size']
 					. ' button_border_' . $a['border'] . ' ';
-								
-			$output .= $a['class'] . '"';
-			$output .= 'href="' . $a['link'] . '"';
-			
-			$output .= 'style="color:' . $a['text_color'] . '; ' 
+
+				$output .= $a['class'] . '"';
+				$output .= 'href="' . $a['link'] . '"';
+
+				$output .= 'style="color:' . $a['text_color'] . '; '
 					. 'background-color: ' . $a['button_color'] . ';"';
-			
-			$output .= '>';
-			$output .= $a['text'];
-			$output .= '</a>';
+
+				$output .= '>';
+				$output .= $a['text'];
+				$output .= '</a>';
 			}
 			else
 			{
 				echo 'Please ensure that you have text for your button';
 			}
-				
-			
+
 			return $output;
 	}
 }
