e34b7d923cf76ac376891c9e58bf40a95d656b69
Will Clardy <will@quexxon.net>
2015-03-09 14:18:03 -0400

    Added form validation

--- js/tinymce/plugin.js
+++ js/tinymce/plugin.js
@@ -5,157 +5,255 @@
             icon: 'icon dashicons-lightbulb',
             type: 'menubutton',
             menu: [
-            {
-                text: 'Button',
-                onclick: function() {
-                    editor.windowManager.open( {
-                        title: 'Insert Button Shortcode',
-                        body: [
-                        {
-                            type: 'textbox',
-                            name: 'id',
-                            label: 'ID'
-                        },
-                        {
-                            type: 'textbox',
-                            name: 'class',
-                            label: 'Class'
-                        },
-                        {
-                            type: 'textbox',
-                            name: 'text',
-                            label: 'Button Text'
-                        },
-                        {
-                            type: 'textbox',
-                            name: 'url',
-                            label: 'Button URL'
-                        },
-                        {
-                            type: 'textbox',
-                            name: 'textcolor',
-                            label: 'Text Color'
-                        },
-                        {
-                            type: 'textbox',
-                            name: 'color',
-                            label: 'Button Color'
-                        },
-                        {
-                            type: 'listbox',
-                            name: 'shape',
-                            label: 'Shape',
-                            'values': [
-                                { text: 'Rounded', value: 'rounded' },
-                                { text: 'Square', value: 'square' }
-                            ]
-                        },
-                        {
-                            type: 'listbox',
-                            name: 'size',
-                            label: 'Size',
-                            'values': [
-                                { text: 'Small', value: 'small' },
-                                { text: 'Medium', value: 'medium' },
-                                { text: 'Large', value: 'large' }
-                            ]
-                        },
-                        {
-                            type: 'radio',
-                            name: 'border',
-                            label: 'Border'
-                        },
-                        ],
-                        onsubmit: function( e ) {
-                            editor.insertContent(
-                                    '[bimg_button id="' + e.data.id +
-                                    '" class="' + e.data.class +
-                                    '" text="' + e.data.text +
-                                    '" url="' + e.data.url +
-                                    '" text_color="' + e.data.textcolor +
-                                    '" button_color="' + e.data.color +
-                                    '" shape="' + e.data.shape +
-                                    '" size="' + e.data.size +
-                                    '" border="' + e.data.border +
-                                    '"]');
-                        }
-                    });
-                }
-            },
-            {
-                text: 'Separator',
-                onclick: function() {
-                    editor.windowManager.open( {
-                        title: 'Insert Separator Shortcode',
-                        body: [
-                        {
-                            type: 'textbox',
-                            name: 'id',
-                            label: 'ID'
-                        },
-                        {
-                            type: 'textbox',
-                            name: 'class',
-                            label: 'Class'
-                        }
-                        ],
-                        onsubmit: function( e ) {
-                            editor.insertContent( '[bimg_separator id="' + e.data.id + '" class="' + e.data.class + '"]');
-                        }
-                    });
-                }
-            },
-            {
-                text: 'Tabs',
-                menu: [
                 {
-                    text: 'Initialize Tabs',
+                    text: 'Button',
                     onclick: function() {
                         editor.windowManager.open( {
-                            title: 'Insert Tabs Shortcode',
+                            title: 'Insert Button Shortcode',
                             body: [
-                            {
-                                type: 'textbox',
-                                name: 'id',
-                                label: 'ID (Required)'
-                            },
-                            {
-                                type: 'textbox',
-                                name: 'class',
-                                label: 'Class'
-                            },
                                 {
                                     type: 'textbox',
-                                    name: 'title',
-                                    label: 'Title of First Tab'
-                                }
+                                    name: 'id',
+                                    label: 'ID'
+                                },
+                                {
+                                    type: 'textbox',
+                                    name: 'class',
+                                    label: 'Class'
+                                },
+                                {
+                                    type: 'textbox',
+                                    name: 'text',
+                                    label: 'Button Text'
+                                },
+                                {
+                                    type: 'textbox',
+                                    name: 'url',
+                                    label: 'Button URL'
+                                },
+                                {
+                                    type: 'textbox',
+                                    name: 'textcolor',
+                                    label: 'Text Color'
+                                },
+                                {
+                                    type: 'textbox',
+                                    name: 'color',
+                                    label: 'Button Color'
+                                },
+                                {
+                                    type: 'listbox',
+                                    name: 'shape',
+                                    label: 'Shape',
+                                    'values': [
+                                        { text: 'Rounded', value: 'rounded' },
+                                        { text: 'Square', value: 'square' }
+                                    ]
+                                },
+                                {
+                                    type: 'listbox',
+                                    name: 'size',
+                                    label: 'Size',
+                                    'values': [
+                                        { text: 'Small', value: 'small' },
+                                        { text: 'Medium', value: 'medium' },
+                                        { text: 'Large', value: 'large' }
+                                    ]
+                                },
+                                {
+                                    type: 'radio',
+                                    name: 'border',
+                                    label: 'Border'
+                                },
                             ],
                             onsubmit: function( e ) {
-                                editor.insertContent( '[bimg_tabs id="' + e.data.id + '" class="' + e.data.class + '"][tab title="' + e.data.title + '"][/tab][/bimg_tabs]' );
+                                editor.insertContent(
+                                    '[bimg_button id="' + e.data.id +
+                                        '" class="' + e.data.class +
+                                        '" text="' + e.data.text +
+                                        '" url="' + e.data.url +
+                                        '" text_color="' + e.data.textcolor +
+                                        '" button_color="' + e.data.color +
+                                        '" shape="' + e.data.shape +
+                                        '" size="' + e.data.size +
+                                        '" border="' + e.data.border +
+                                        '"]');
                             }
                         });
                     }
                 },
                 {
-                    text: 'Add Tab',
+                    text: 'Grid',
+                    menu: [
+                        {
+                            text: 'Add Row',
+                            onclick: function() {
+                                editor.windowManager.open( {
+                                    title: 'Insert Row Shortcode',
+                                    body: [
+                                        {
+                                            type: 'textbox',
+                                            name: 'id',
+                                            label: 'ID'
+                                        },
+                                        {
+                                            type: 'textbox',
+                                            name: 'class',
+                                            label: 'Class'
+                                        },
+                                        {
+                                            type: 'radio',
+                                            name: 'equal',
+                                            label: 'Equal Height Columns?'
+                                        }
+                                    ],
+                                    onsubmit: function( e ) {
+                                        if( e.data.id == '' && e.data.equal == true ) {
+                                            editor.windowManager.alert('You must specify an ID to use equal height columns.');
+                                            return false;
+                                        }
+                                        editor.insertContent(
+                                            '[bimg_row id="' + e.data.id +
+                                                '" class="' + e.data.class +
+                                                '" equal="' + e.data.equal +'"][/bimg_row]' );
+                                    }
+                                });
+                            }
+                        },
+                        {
+                            text: 'Add Column',
+                            onclick: function() {
+                                editor.windowManager.open( {
+                                    title: 'Insert Column Shortcode',
+                                    body: [
+                                        {
+                                            type: 'textbox',
+                                            name: 'id',
+                                            label: 'ID'
+                                        },
+                                        {
+                                            type: 'textbox',
+                                            name: 'class',
+                                            label: 'Class'
+                                        },
+                                        {
+                                            type: 'textbox',
+                                            name: 'columns',
+                                            label: 'Columns in Row (1-12)'
+                                        },
+                                        {
+                                            type: 'textbox',
+                                            name: 'width',
+                                            label: 'Column Width (1-12)'
+                                        }
+                                    ],
+                                    onsubmit: function( e ) {
+                                        var range = new Array('1','2','3','4','5','6','7','8','9','10','11','12','13');
+                                        if(!(e.data.columns in range)) {
+                                            editor.windowManager.alert('"Columns in Row" must be an integer between 1 and 12.');
+                                            return false;
+                                        }
+                                        if(!(e.data.width in range)) {
+                                            editor.windowManager.alert('"Column Width" must be an integer between 1 and 12.');
+                                            return false;
+                                        }
+                                        editor.insertContent(
+                                            '[bimg_col id="' + e.data.id +
+                                                '" class="' + e.data.class +
+                                                '" columns="' + e.data.columns +
+                                                '" width="' + e.data.width + '"][/bimg_col]' );
+                                    }
+                                });
+                            }
+                        }
+                    ]
+                },
+                {
+                    text: 'Separator',
                     onclick: function() {
                         editor.windowManager.open( {
-                            title: 'Insert Additional Tab',
+                            title: 'Insert Separator Shortcode',
                             body: [
-                            {
-                                type: 'textbox',
-                                name: 'title',
-                                label: 'Title'
-                            }
+                                {
+                                    type: 'textbox',
+                                    name: 'id',
+                                    label: 'ID'
+                                },
+                                {
+                                    type: 'textbox',
+                                    name: 'class',
+                                    label: 'Class'
+                                }
                             ],
                             onsubmit: function( e ) {
-                                editor.insertContent( '[tab title="' + e.data.title + '"][/tab]' );
+                                editor.insertContent( '[bimg_separator id="' + e.data.id + '" class="' + e.data.class + '"]');
                             }
                         });
                     }
+                },
+                {
+                    text: 'Tabs',
+                    menu: [
+                        {
+                            text: 'Initialize Tabs',
+                            onclick: function() {
+                                editor.windowManager.open( {
+                                    title: 'Insert Tabs Shortcode',
+                                    body: [
+                                        {
+                                            type: 'textbox',
+                                            name: 'id',
+                                            label: 'ID (Required)'
+                                        },
+                                        {
+                                            type: 'textbox',
+                                            name: 'class',
+                                            label: 'Class'
+                                        },
+                                        {
+                                            type: 'textbox',
+                                            name: 'title',
+                                            label: 'Title of First Tab'
+                                        }
+                                    ],
+                                    onsubmit: function( e ) {
+                                        if( e.data.id === '') {
+                                            editor.windowManager.alert('You must specify an ID.');
+                                            return false;
+                                        }
+                                        if( e.data.title === '') {
+                                            editor.windowManager.alert('A tab without a title would be confusing, no?');
+                                            return false;
+                                        }
+                                        editor.insertContent( '[bimg_tabs id="' + e.data.id + '" class="' + e.data.class + '"][tab title="' + e.data.title + '"][/tab][/bimg_tabs]' );
+                                    }
+                                });
+                            }
+                        },
+                        {
+                            text: 'Add Tab',
+                            onclick: function() {
+                                editor.windowManager.open( {
+                                    title: 'Insert Additional Tab',
+                                    body: [
+                                        {
+                                            type: 'textbox',
+                                            name: 'title',
+                                            label: 'Title'
+                                        }
+                                    ],
+                                    onsubmit: function( e ) {
+                                        if( e.data.title === '') {
+                                            editor.windowManager.alert('A tab without a title would be confusing, no?');
+                                            return false;
+                                        }
+                                        editor.insertContent( '[tab title="' + e.data.title + '"][/tab]' );
+                                    }
+                                });
+                            }
+                        }
+                    ]
                 }
-                ]
-            }
             ]
         });
     });
