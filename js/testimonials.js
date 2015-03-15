ba18ad487b1ba4e2d5595dcaf2dba7a055c0f1ba
ChrisBurgin <chrisburgin95@gmail.com>
2015-03-15 11:53:34 -0400

    Testimonials Final (Will Update TinyMCE)

--- js/testimonials.js
+++ js/testimonials.js
@@ -1,37 +1,34 @@
-//see bimg-testimonials php file for to-do list
+//see bimg-testimonials 	php file for to-do list
 
-function rotateTestimonials() {
-
+function rotateTestimonials(Delay) {
 	var elements = document.getElementsByClassName('testimonial_slider'); //creates a nodelist of all the objects mathing the class
 	var i = 0; // used to keep track of where we are at throught the loop
 	var total = elements.length - 1;
+	var delay = Delay;
 
 if (total != 0 )
 {
-	rotate ();
+	rotate();
 }
-
-	function rotate () {
-
-		if (i <= total) {
-			if (i == 0) //the first iteration of a loop, fades out the last item and fades in the first item
-			{
+	function rotate(){
+		if (i <= total){
+			if (i == 0){ //the first iteration of a loop, fades out the last item and fades in the first item
+			
 				jQuery(elements.item(total)).fadeOut(800); 
 				jQuery(elements.item(i)).fadeIn(800).delay();
 			}
-			else 
-			{ // fades out the previous item and fades in the current item
+			else {
+			 // fades out the previous item and fades in the current item
 				jQuery(elements.item(i - 1)).fadeOut(800);
 				jQuery(elements.item(i)).fadeIn(800);
 			}
 			i++; 
-			setTimeout(rotate , 8000); //waits a set amount of time and then moves on
+			setTimeout(rotate , delay); //waits a set amount of time and then moves on
 		}
-		else 
-		{ //resets the loop using an else so we dont waste unnesseary processing on an if statement every single time
+		else { 
+		 //resets the loop using an else so we dont waste unnesseary processing on an if statement every single time
 			i = 0;
 			rotate ();
 		}
-		
 	}
 }
--- shortcodes/bimg-testimonials.php
+++ shortcodes/bimg-testimonials.php
@@ -1,74 +1,71 @@
 <?php
-/*TO-DO
- * I will do this list on monday
- * Custom time (transition and overall)
- * Clean Up Code
- * Error Catching
- * Update tinymce
- * Add Title
-*/
- 
- 
- 
 class BIMGTestimonial {
-    public function __construct()
-    {
-        add_action( 'wp_enqueue_scripts', array( $this, 'enqueue_shortcode_scripts' ) );
-        add_shortcode( 'bimg_testimonial', array( $this, 'shortcode' ) );
-    }
+	public function __construct()
+	{
+		add_action( 'wp_enqueue_scripts', array( $this, 'enqueue_shortcode_scripts' ) );
+		add_shortcode( 'bimg_testimonial', array( $this, 'shortcode' ) );
+	}
 
-    public function enqueue_shortcode_scripts()
-    {
+	public function enqueue_shortcode_scripts()
+	{
 	    wp_enqueue_style( 'testimonials', plugins_url( 'bimg-wp-shortcodes/css/testimonials.css' ) );
-        wp_enqueue_script( 'jquery' );
-        wp_enqueue_script( 'wait', plugins_url( 'bimg-wp-shortcodes/js/wait.js' ) );
-        wp_enqueue_script( 'testimonials', plugins_url( 'bimg-wp-shortcodes/js/testimonials.js' ) );
-        
-    }
+		wp_enqueue_script( 'jquery' );
+		wp_enqueue_script( 'wait', plugins_url( 'bimg-wp-shortcodes/js/wait.js' ) );
+		wp_enqueue_script( 'testimonials', plugins_url( 'bimg-wp-shortcodes/js/testimonials.js' ) );
 
-    public function shortcode( $atts, $content = null )
-    {
-        $a = shortcode_atts( array(
-            'class' => null,
-            'id' => null,
-            'height' =>  150,
-            'width' => 300,
-            'post_type' => 'testimonial',
-            'category_name' => 'testimonials',
-        ), $atts, 'bimg_testimonial' );
+	}
 
-        return $this->build_testimonial( $a['class'], $a['id'], $a['height'], $a['width'], $a['post_type'],$a['category_name'], $content );
-    }
+	public function shortcode( $atts, $content = null )
+	{
+		$a = shortcode_atts( array(
+			'class' => null,
+			'id' => null,
+			'height' => 150,
+			'width' => 300,
+			'delay' => 8000,
+			'post_type' => 'testimonial',
+			'category_name' => 'testimonials',
+		), $atts, 'bimg_testimonial' );
 
-    public function build_testimonial( $class, $id, $height, $width, $post_type, $category, $content )
-    {
+		return $this->build_testimonial( $a['class'], $a['id'], $a['height'], $a['width'], $a['delay'], $a['post_type'],$a['category_name'], $content );
+	}
+
+	public function build_testimonial( $class, $id, $height, $width, $delay, $post_type, $category, $content )
+	{
 	    $output = '<div class="testimonail_container" style="height:' . $height . 'px; width:' . $width . 'px;">';
-	     
-        $args = array(); // Args for the Loop
+
+		$args = array(); // Args for the Loop
 		if ( $post_type !== null ) {
 			$args['post_type'] = $post_type;
 		}
 		if ( $category !== null ) {
 			$args['category_name'] = $category;
 		}
-	   
-	    $loop = new WP_Query( $args );   
-        while ( $loop->have_posts() ) 
-        {
-	      
+	    $loop = new WP_Query( $args );
+		while ( $loop->have_posts() ) {
+
 			$loop->the_post();
 			$testimonial_content = apply_filters( 'the_content', get_the_content() );
-			$output .= '<div class="testimonial_slider" style="height:' . $height . 'px; width:' . $width . 'px;">';
+			$testimonial_title = apply_filters( 'the_title', get_the_title() );
+			$output .= '<div class="testimonial_slider';
+			if ( isset($class) ) {
+				$output .= ' ' . $class . ' ';
+			}
+			$output .= '" style="height:' . $height . 'px; width:' . $width . 'px;">';
 			$output .= $testimonial_content;
+			$output .= '<div class="testimonials_title">' . $testimonial_title . '</div>';
 			$output .= '</div>';
+
+		}
+		if ( $loop->have_posts() ) {
+	    	$output .= '</div>';
+			$output .= '<script> jQuery( document ).ready(function() { ';
+			$output .= ' rotateTestimonials(' . $delay  . ');';
+			$output .= ' });</script>';
 		}
 
-	    $output .= '</div>';
-	    $output .= '<script> jQuery( document ).ready(function() {
-	     rotateTestimonials(); 
-	     });</script>';
 	    wp_reset_postdata();
 	    return $output;
-    }
+	}
 }
 
